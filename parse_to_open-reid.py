f7591de7729f3c556f74766ed2a6ce70422c639d
Floris <floris.defeyter@kuleuven.be>
2017-10-03 16:28:33 +0200

    Complete argument checking

--- parse_to_open-reid.py
+++ parse_to_open-reid.py
@@ -3,28 +3,53 @@ import re
 
 special_chars = {
     'image_id': 'i',
-    'camera_id' : 'c',
-    'person_id' : 'p',
-    'deeper_level' : '/',
-    'escape' : '\\',
-    'dont_care' : '_'
+    'camera_id': 'c',
+    'person_id': 'p',
+    'deeper_level': '/',
+    'escape': '\\',
+    'dont_care': '_'
 }
 
 # Get the positions of all the special characters
 def get_special_chars_positions(string):
-    return {name: [m.start() for m in re.finditer(re.escape(char), string)]
-           for name, char in special_chars.items()}
+    return {char: [m.start() for m in re.finditer(re.escape(char), string)]
+            for _, char in special_chars.items()}
 
 # Check if the string has the correct format and that all paths are reachable
 def generic_path(string):
+    char_positions = get_special_chars_positions(string)
     # The generic path must contain a reference to the image-id ("i")
-    if not "i" in string:
-        argparse.ArgumentTypeError('{} does not contain "i".'.format(string))
-    return "{}".format(string)
+    if len(char_positions[special_chars['image_id']]) is 0:
+        raise argparse.ArgumentTypeError(
+            'There is no reference to the image-id ("{}")'
+            .format(special_chars['image_id']))
+
+    # The path cannot contain more than one image-id, person-id or camera-id
+    if len(char_positions[special_chars['image_id']]) > 1:
+        raise argparse.ArgumentTypeError(
+            'Only one image_id reference can be given.')
+    if len(char_positions[special_chars['camera_id']]) > 1:
+        raise argparse.ArgumentTypeError(
+            'Only one camera_id reference can be given.')
+    if len(char_positions[special_chars['person_id']]) > 1:
+        raise argparse.ArgumentTypeError(
+            'Only one person_id reference can be given.')
+
+    # Two deeper-level characters after each other are not allowed
+    pos = char_positions[special_chars['deeper_level']]
+    sorted_pos, size = sorted(pos), len(pos)
+    if min([sorted_pos[i + 1] - sorted_pos[i]
+            for i in range(size) if i + 1 < size]) == 1:
+        raise argparse.ArgumentTypeError(
+            'Two successive "{}" characters are not allowed.'
+        .format(special_chars['deeper_level']))
+
+    return string
 
 
-parser = argparse.ArgumentParser(description="Transform a dataset into the open-reid format")
-parser.add_argument('--input_format', help='The input format.\
+parser = argparse.ArgumentParser(
+    description="Transform a dataset into the open-reid format")
+parser.add_argument('--ifor', help='The input format.\
                     Use "{camera_id}" for camera-id, "{person_id}" for person-id, "{image_id}" for image-id, "{deeper_level}" for a deeper folder level and "{dont_care}" for a don\'t care.  Use "{escape}" to escape any of the special characters.\
                     The final part of the path is assumed to be the image.\
                     An id character ("{camera_id}", "{person_id}", "{image_id}") can only occur once.\
@@ -32,10 +57,11 @@ parser.add_argument('--input_format', help='The input format.\
                     If no camera-id (or person-id) is specified, the images will be assumed to all have the same camera-id(resp. person-id).'.format(**special_chars),
                     required=True, type=generic_path)
 
-parser.add_argument('--top_level', help='Path to the top level of the dataset.\
+parser.add_argument('--top', help='Path to the top level of the dataset.\
                     Default: current path.', type=str)
 args = parser.parse_args()
-print(get_special_chars_positions(args.input_format))
+
+
 
 # Iterate through all camera-id's
 
@@ -43,8 +69,3 @@ print(get_special_chars_positions(args.input_format))
 
 # Iterate through all image-id's
 
-# How many levels deeper than the current level do we need to go to find the id
-# specified by the character?
-
-
-# def find_id_level(id_char, input_format):
