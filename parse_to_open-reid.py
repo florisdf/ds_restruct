68bebc4a191b28d1cf3fc414f7ea55dd8795e29e
Floris <floris.defeyter@kuleuven.be>
2017-10-03 12:12:54 +0200

    Added char counter

--- parse_to_open-reid.py
+++ parse_to_open-reid.py
@@ -1,26 +1,42 @@
 import argparse
+import re
+
+special_chars = {
+    'image_id': 'i',
+    'camera_id' : 'c',
+    'person_id' : 'p',
+    'deeper_level' : '/',
+    'escape' : '\\',
+    'dontcare' : '_'
+}
+
+# Count number of times a special character is inside a generic string
+def count_special_chars(string):
+    return {name: len(re.findall(r'([^\\]|^){}'.format(char)))
+                                          for name, char in special_chars.items()}
 
 # Check if the string has the correct format and that all paths are reachable
 def generic_path(string):
+    # The generic path must contain a reference to the image-id ("i")
+    if not "i" in string:
+        argparse.ArgumentTypeError('{} does not contain "i".'.format(string))
+    #
 
 
-parser = argparse.ArgumentParser(description="Transform a dataset into the\
-                                 open-reid format")
-parser.add_argument('--input_format', help='The input format. Use "c" for\
-                    camera-id, "p" for person-id, "i" for image-id, "/" for\
-                    a deeper folder level and "_" for a don\'t care.\
-                    Use "\\" to escape any of the special characters.\
+parser = argparse.ArgumentParser(description="Transform a dataset into the open-reid format")
+parser.add_argument('--input_format', help='The input format.\
+                    Use "{camera_id}" for camera-id, "{person_id}" for person-id, "{image_id}" for image-id, "{deeper_folder}" for a deeper folder level and "{dont_care}" for a don\'t care.  Use "{escape}" to escape any of the special characters.\
                     The final part of the path is assumed to be the image.\
-                    An id character ("c", "p", "i") can only occur once. The\
-                    camera-id ("c") and person-id ("p") are optional, while the
-                    image-id ("i") must be in the given string. If no camera-id
-                    (or person-id) is specified, the images will be assumed to\
-                    all have the same camera-id (resp. person-id).',
+                    An id character ("{camera_id}", "{person_id}", "{image_id}") can only occur once.\
+                    Camera-id ("{camera_id}") and person-id ("{person_id}") are optional, while th image-id("{image_id}") must be in the given string.\
+                    If no camera-id (or person-id) is specified, the images will be assumed to all have the same camera-id(resp. person-id).'
+                    .format(**special_chars),
                     type=str, required=True, type=generic_path)
-parser.add_argument('--top_level', help='Path to the top level of the\
-                    dataset. Default: current path.', type=str)
+
+parser.add_argument('--top_level', help='Path to the top level of the dataset.\
+                    Default: current path.', type=str)
 args = parser.parse_args()
-print(args)
+print(count_special_chars(input_format))
 
 # Iterate through all camera-id's
 
@@ -30,4 +46,6 @@ print(args)
 
 # How many levels deeper than the current level do we need to go to find the id
 # specified by the character?
-def find_id_level(id_char, input_format):
+
+
+# def find_id_level(id_char, input_format):
