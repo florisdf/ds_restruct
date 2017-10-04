35ec19d70ac2bad12bc5cf3f57b0abe8ed139587
Floris <floris.defeyter@kuleuven.be>
2017-10-04 11:25:27 +0200

    Rename deeper-path to slash

--- parse_to_open-reid.py
+++ parse_to_open-reid.py
@@ -7,7 +7,7 @@ special_chars = {
     'image_id': 'i',
     'camera_id': 'c',
     'person_id': 'p',
-    'deeper_level': '/',
+    'slash': '/',
     'escape': '\\',
     'dont_care': '*'
 }
@@ -38,19 +38,19 @@ def generic_path(string):
             'Only one person_id reference can be given.')
 
     # Two deeper-level characters after each other are not allowed
-    pos = char_positions[special_chars['deeper_level']]
-    sorted_pos, size = sorted(pos), len(pos)
+    chars_positions = char_positions[special_chars['slash']]
+    sorted_pos, size = sorted(chars_positions), len(pos)
     if min([sorted_pos[i + 1] - sorted_pos[i]
             for i in range(size) if i + 1 < size]) == 1:
         raise argparse.ArgumentTypeError(
             'Two successive "{}" characters are not allowed.'
-        .format(special_chars['deeper_level']))
+        .format(special_chars['slash']))
 
     # The generic path cannot start with a deeper-level character
     if min(pos) is 0:
         raise argparse.ArgumentTypeError(
             'It is not allowed to start the input format with a "{}".'
-            .format(special_chars['deeper_level']))
+            .format(special_chars['slash']))
 
     return string
 
@@ -64,7 +64,7 @@ def path(string):
 parser = argparse.ArgumentParser(
     description="Transform a dataset into the open-reid format")
 parser.add_argument('--ifor', help='The input format.\
-                    Use "{camera_id}" for camera-id, "{person_id}" for person-id, "{image_id}" for image-id, "{deeper_level}" for a deeper folder level and "{dont_care}" for a don\'t care.  Use "{escape}" to escape any of the special characters.\
+                    Use "{camera_id}" for camera-id, "{person_id}" for person-id, "{image_id}" for image-id, "{slash}" for a deeper folder level and "{dont_care}" for a don\'t care.  Use "{escape}" to escape any of the special characters.\
                     The final part of the path is assumed to be the image.\
                     An id character ("{camera_id}", "{person_id}", "{image_id}") can only occur once.\
                     Camera-id ("{camera_id}") and person-id ("{person_id}") are optional, while th image-id("{image_id}") must be in the given string.\
@@ -72,16 +72,24 @@ parser.add_argument('--ifor', help='The input format.\
                     required=True, type=generic_path)
 
 parser.add_argument('--top', help='Path to the top level of the dataset.\
-                    Default: current path.', type=str)
+                    Default: current path.', type=path)
 args = parser.parse_args()
 
-def get_ids(id_char, top_dir, general_path):
-    pos = get_special_chars_positions(general_path)
-    char_pos = pos[id_char]
-    if len(char_pos) == 0 return None
-    lev_pos = sorted(pos[special_chars['deeper_level']])
-    char_lev = bisect.bisect_left(lev_pos, char_pos[0])
-    # Dig through all dir levels recursively
+# Get all the id values corresponding to the given id character
+def get_id_values(id_char, top_dir, general_path):
+    chars_positions = get_special_chars_positions(general_path)
+    id_positions = chars_positions[id_char]
+    assert len(id_positions) is not 0
+    slash_positions = sorted(chars_positions[special_chars['slash']])
+
+    if len(slash_positions) is 0 or
+    bisect.bisect_left(slash_positions, id_positions[0]) == 0:
+        # The id will be found in this directory
+        return None
+    else:
+        # Recursive call
+        return None
+
 
 def get_identities(top_dir, general_path):
     # Step through all the levels in the given path
